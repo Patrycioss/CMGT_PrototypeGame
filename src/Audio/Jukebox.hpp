6c2346551e72a8a8df30c0e6b1f80b85d021224c
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2022-11-09 16:47:31 +0100

    CMake updates

--- src/Audio/Jukebox.hpp
+++ src/Audio/Jukebox.hpp
@@ -5,10 +5,16 @@ class Jukebox
 {
 private:
 	Jukebox();
+	
+	std::string idleTracks_[3];
+	std::string fightTracks_[3];
+
 public:
 	
 	sf::Sound buttonClick;
 	sf::Sound exit;
+	
+	sf::Music music;
 
 	static Jukebox& get();
 };
